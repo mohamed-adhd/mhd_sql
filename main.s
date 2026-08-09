a83ff13cb84daf60c0bbd85ed35f0b723bbf02a9
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-09 19:42:13 +0000

    fstat call added

--- main.s
+++ main.s
@@ -9,18 +9,26 @@ filelen equ $ -filename
 align 8
 page_buf: resb 65536
 header_buf: resb 100
+temp: resb 144
 .text
 	mov rsi,0
 	mov rdi,[rsp+16]
 	mov rdx,0
 	mov rax,2
 	syscall;openin
+	mov ebx,rax
+	mov ecx,temp
+	push rax
+	mov rax,5
+	syscall
+	pop rax
 	mov rdi,rax
 	mov rsi,header_buf
 	mov rdx,2
 	mov rcx,16
-	call readnbytes
-	 
+	call readnbytes; so here it should start reading page by page till a page type bytes are 0x0D
+	
+		 
 
 
 

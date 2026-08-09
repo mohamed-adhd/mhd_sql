e882b357d5812bb766a510dc84ad7fc7b83cf8d2
mohamed amdouni <midouamdouni4@gmail.com>
2026-08-09 18:15:54 +0000

    so far added the file open/reading

--- main.s
+++ main.s
@@ -1,5 +1,34 @@
 .rodata
 
-;soon
+filename:
+    db "test.sqlite",10
+
+filelen equ $ -filename
 
 .text
+
+
+
+open_file:
+	mov rsi,0
+	mov rdi,filename
+	mov rdx,0
+	mov rax,2
+	syscall
+sys_read:
+	mov rax,8
+	mov rsi,rcx
+	mov rdx,0
+	syscall
+	pop rdx
+	pop rsi
+	pop rax
+
+
+
+
+
+
+
+
+
